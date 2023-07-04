0d7403c42c0493b158a136772ef81118cdd3d1ca
Lucas Duarte <102632990+lucxsduarte@users.noreply.github.com>
2023-07-04 17:17:22 -0300

    começando exercicio imc

--- js/imc.js
+++ js/imc.js
@@ -1,11 +1,20 @@
 function submitForm(){
-    nome = document.getElementsByName(inputName).value;
-    sexo = document.getElementsByName(inputSex).value;
-    imc = (document.getElementById(txtWeight).value / (document.getElementById(txtHeight).value * document.getElementById(txtHeight).value));
-    console.log(nome);
-    console.log(sexo);
-    console.log(imc);
-    msg = `Nome: ${nome}\nSexo: ${sexo}\nIMC: ${imc}`;
+    nome = document.getElementById("txtName").value;
+    sexo = document.querySelector('input[name="inputSex"]:checked').value;
+    imc = (document.getElementById("txtWeight").value / (document.getElementById("txtHeight").value * document.getElementById("txtHeight").value));
+    const imcFormatado = Math.round(imc * 100) / 100;
+    msg = `Nome: ${nome}\nSexo: ${sexo}\nIMC: ${imcFormatado}\n`;
+    let status = "";
+    if (imcFormatado < 18.5) {
+        status = "Status: MAGREZA";
+    } else if (imcFormatado >= 18.5 && imcFormatado <= 24.9) {
+        status = "Status: NORMAL";
+    } else if (imcFormatado > 24.9 && imcFormatado <=30) {
+        status = "Status: SOBREPESO";   
+    } else if (imcFormatado > 30){
+        status = "Status: OBESIDADE";
+    }
+    msg += status;
     document.getElementById("txtResult").value = msg;
     return false;
 }
